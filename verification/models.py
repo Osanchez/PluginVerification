335101a3770eada3ebbce95b5dcfcc74ed426b8d
Osanchez <osanchez@umass.edu>
2019-03-03 17:57:35 -0500

    added authentication and blacklist models. configured admin page

--- verification/models.py
+++ verification/models.py
@@ -2,12 +2,24 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 
-class Authentic(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+class Authentication(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     plugin_name = models.CharField(max_length=100)
-    date_purchased = models.DateField(auto_now_add=True)
+    date_purchased = models.DateTimeField(default=timezone.now)
     authentic_key = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.user.__str__()
+
+
+class Blacklist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.__str__()
