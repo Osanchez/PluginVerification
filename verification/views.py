372427f6f9fd6f066fc29bdad168495f9a8b8009
Osanchez <osanchez@umass.edu>
2019-03-22 04:17:58 -0400

    implemented plugin verification methods. testing.

--- verification/views.py
+++ verification/views.py
@@ -5,13 +5,19 @@ from .verify import Verification
 
 # Create your views here.
 
+# TODO: take passkey in the url, or come up with a better solution for verification
+
 
 def verification(request, uuid4):
     print(uuid4)
 
     model = Authentication
-    generated_key = Verification.hash_id('test')
-    degenerated_key = Verification.check_id(generated_key, 'test')
+
+    passphrase = 'test'
+    passphrase_authenticate = 'test1'  # this is what the user will enter to authenticate
+
+    generated_key = Verification.hash_id(passphrase)
+    degenerated_key = Verification.check_id(generated_key, passphrase_authenticate)
 
     if model.objects.filter(authentic_key=uuid4).exists():
         retrieved = model.objects.get(authentic_key=uuid4)
@@ -19,22 +25,18 @@ def verification(request, uuid4):
         data = {
             'plugin_name': retrieved.plugin_name,
             'owner': retrieved.owner,
-            'is_authentic': '',  # call verification tools
-            'returned_key': uuid4,
-            'hash_test_phrase': 'test',
+            'entered_phrase': passphrase,
             'generated_hashkey': generated_key,
-            'degenerated_hashkey_matches': degenerated_key
+            'is_authentic': degenerated_key,
         }
 
     else:
         data = {
             'plugin_name': 'N/A',
             'owner': 'N/A',
-            'is_authentic': 'False',  # call verification tools
-            'returned_key': uuid4,
-            'hash_test_phrase': 'test',
+            'entered_phrase': passphrase,
             'generated_hashkey': generated_key,
-            'degenerated_hashkey_matches': degenerated_key
+            'is_authentic': degenerated_key,
         }
 
     return JsonResponse(data)
